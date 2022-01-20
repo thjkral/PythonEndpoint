b2c0b8e770b001edbe18b0091b804fe0bbcea4e9
FelixvL <digitaleservice@gmail.com>
2022-01-20 15:25:13 +0100

    felix bestand erbij MET link naar ieder ander zijn bestand

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ Created on Thu Jan 20 12:14:22 2022
 
 from flask import Flask
 import endpointTest
+import BestandFreek as fr
+import tomsFile as tf
+import bestandFelix as bf
 
 app = Flask(__name__)
 
@@ -16,9 +19,19 @@ def end_point_test():
     message = endpointTest.readFile()
     return message
 
+@app.route("/mijnfunctie")
+def end_point_test_mijnfunctie():
+    fr.Freek()
+    return "functie van Freek"
 
+@app.route("/functietom")
+def end_point_functietom():
+    tf.toms_functie()
+    return "functie van Tom"
 
-
+@app.route("/functiefelix")
+def end_point_functiefelix():
+    return bf.felixProbeert()
 
 
 
