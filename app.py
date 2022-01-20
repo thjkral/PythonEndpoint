97f38d2536f58893a63fa10c1fc01ce7e18b7eae
thjkral <thjkral@gmail.com>
2022-01-20 12:57:03 +0100

    Flask app toegevoegd

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 12:14:22 2022
+
+@author: tom
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello World!</p>"
+
+
+
