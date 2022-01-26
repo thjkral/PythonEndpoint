8a1a40b28bb28bd571cb9362ef563f2f672287c3
NeonChicken <lcvandort@hotmail.com>
2022-01-26 13:04:06 +0100

    retrying app route render_template without importing WebcamBarcodeReader

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, render_template, Response, redirect
 import pandas as pd
 import json
 import BestandFreek as fr
-import WebcamBarcodeReader as wbr
+# import WebcamBarcodeReader as wbr
 
 app = Flask(__name__)
 
@@ -42,28 +42,28 @@ def cam():
   return render_template("cam.html")
 
 
-@app.route("/video_feed", methods = ["POST", "GET"])  # Video feed, as Response html page, non-visitable
-def video_feed():
-    return Response(wbr.gen(),
-        mimetype="multipart/x-mixed-replace; boundary=frame")
-
-
-@app.route("/scanner", methods = ["POST", "GET"])  # Scanner landing page
-def scanner():
-  return render_template("scanner.html")
-
-
-@app.route("/restart", methods = ["POST", "GET"])  #
-def restart():
-  wbr.code = 0
-  return redirect("/cam")
-
-
-@app.route("/result", methods = ["POST", "GET"])
-def result():
-  if wbr.code:
-    return render_template("result.html", barcode=wbr.code)
-  else:
-    return redirect("scanner")
+# @app.route("/video_feed", methods = ["POST", "GET"])  # Video feed, as Response html page, non-visitable
+# def video_feed():
+#     return Response(wbr.gen(),
+#         mimetype="multipart/x-mixed-replace; boundary=frame")
+#
+#
+# @app.route("/scanner", methods = ["POST", "GET"])  # Scanner landing page
+# def scanner():
+#   return render_template("scanner.html")
+#
+#
+# @app.route("/restart", methods = ["POST", "GET"])  #
+# def restart():
+#   wbr.code = 0
+#   return redirect("/cam")
+#
+#
+# @app.route("/result", methods = ["POST", "GET"])
+# def result():
+#   if wbr.code:
+#     return render_template("result.html", barcode=wbr.code)
+#   else:
+#     return redirect("scanner")
 
 # END Barcode Webcam Scanner
