6def54194cd8b1bc2fd8b38a3cb09f77b859380e
NeonChicken <lcvandort@hotmail.com>
2022-01-26 10:56:14 +0100

    Added barcode scanner at /cam

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ Created on Thu Jan 20 12:14:22 2022
 """
 
 
-from flask import Flask
+from flask import Flask, render_template, Response, redirect
 import pandas as pd
 import json
 import BestandFreek as fr
+import WebcamBarcodeReader as wbr
 
 app = Flask(__name__)
 
@@ -34,7 +35,38 @@ def bcode_lookup(bcode):
    return bcode.prod_lookup(bcode)
 
 
+# START Barcode Webcam Scanner
+
+@app.route("/cam")  # Barcode webcam scanner
+def cam():
+  return render_template("cam.html")
 
 
+@app.route("/video_feed", methods = ["POST", "GET"])  # Video feed, as Response html page, non-visitable
+def video_feed():
+    return Response(wbr.gen(),
+        mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
+@app.route("/scanner", methods = ["POST", "GET"])  # Scanner landing page
+def scanner():
+  return render_template("scanner.html")
+
+
+@app.route("/restart", methods = ["POST", "GET"])  #
+def restart():
+  wbr.code = 0
+  return redirect("/cam")
+
+
+@app.route("/result", methods = ["POST", "GET"])
+def result():
+  if wbr.code:
+    return render_template("result.html", barcode=wbr.code)
+  else:
+    return redirect("scanner")
+
+# END Barcode Webcam Scanner
+
+if __name__ == '__main__':
+  app.run(debug=True, port=5001)
