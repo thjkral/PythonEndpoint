5f75a0a4d249246477b303dcf90d70ad16af6a3c
NeonChicken <lcvandort@hotmail.com>
2022-01-26 17:25:16 +0100

    Problem = pyzbar, trying to import without running any pyzbar funtions

--- WebcamBarcodeReader.py
+++ WebcamBarcodeReader.py
@@ -1,5 +1,5 @@
 import cv2
-# from pyzbar import pyzbar as pb
+import pyzbar.pyzbar as pb
 
 # from imutils.video import VideoStream
 # https://www.pyimagesearch.com/2019/09/02/opencv-stream-video-to-web-browser-html-page/
