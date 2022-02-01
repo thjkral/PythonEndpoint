c1ddc0fc7c69bf745d571b627d9d4924c4919066
NeonChicken <lcvandort@hotmail.com>
2022-02-01 10:05:23 +0100

    local changes

--- WebcamBarcodeReader.py
+++ WebcamBarcodeReader.py
@@ -1,6 +1,9 @@
 import cv2
 import pyzbar.pyzbar as pb
 
+import imagezmq
+import imutils
+
 # from imutils.video import VideoStream
 # https://www.pyimagesearch.com/2019/09/02/opencv-stream-video-to-web-browser-html-page/
 
